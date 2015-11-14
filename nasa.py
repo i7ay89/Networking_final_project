009e6873257e3b72d852320f43f5511791bd0027
Itay <i7ay89@gmail.com>
2015-11-14 01:59:30 -0800

    - Started working on the nasa module

--- nasa.py
+++ nasa.py
@@ -1,4 +1,4 @@
-__author__ = 'user'
+# Itay Parnafes 302698899
 
 import sys
 import time
@@ -8,7 +8,9 @@ default_udp_port    = 10000
 loopback_address    = '127.0.0.1'
 default_filename    = 'input.txt'
 block_size          = 500 - 20 - 8
-seq_mask = 0xffffffffffff
+seq_mask            = 0xffffffffffff
+handshake_message   = 'yo'
+default_wait_time   = 2
 
 
 class Packet(object):
@@ -37,12 +39,41 @@ def read_file_and_fragment_data():
         i += 1
     return fragmented_data
 
+
 def extract_sequence_number(packet_data):
     return packet_data ^ seq_mask
 
+
 def init_socket():
-    return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setblocking(0)
+    return sock
+
+
+def handshake(relay_socket, ip_port=(loopback_address, default_udp_port)):
+    relay_socket.setblocking(1)
+    reply = ''
+    while not reply:
+        try:
+            relay_socket.send_to(handshake_message, ip_port)
+            relay_socket.settimeout(5)
+            reply = relay_socket.recvfrom(block_size)[0]
+
+        except socket.timeout:
+            pass
+
+    if reply == "hello":
+        relay_socket.setblocking(0)
+        return True
+    return False
 
 
 def main():
+    relay_ip_address = sys.argv[1]
     sock = init_socket()
+    connection_established = handshake(sock, (relay_ip_address, default_udp_port))
+
+    if not connection_established:
+        print '[!] Was unable to establish connection with relay server...'
+        sys.exit()
+
