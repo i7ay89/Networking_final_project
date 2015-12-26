c58aac629e45a63b7ed368e1635c98757ea6bb43
i7ay89 <i7ay89@gmail.com>
2015-12-26 11:45:24 -0800

    Almost done

--- nasa.py
+++ nasa.py
@@ -13,7 +13,9 @@ block_size                             = 500 - 20 - 8 - 2
 received_buffer_size                   = 20 + 8 + 4
 handshake_message                      = 'yo'
 default_wait_time                      = 2
+default_time_between_send              = 0.1
 handshake_word                         = 'hello'
+done_sequence_number                   = 255
 
 
 class Packet(object):
@@ -29,33 +31,36 @@ class Packet(object):
         self.__data = packet_header + self.__raw_data
 
     def send_packet(self, relay_socket, ip_port=(loopback_address, default_udp_port)):
-        relay_socket.send_to(self.__data, ip_port)
+        relay_socket.sendto(self.__data, ip_port)
 
+    def data(self):
+        return self.__raw_data
 
 def read_file_and_fragment_data():
+    global default_filename
+
     f = open(default_filename, 'rb')
     fragmented_data = []
     data = f.read(block_size)
-    i = 0
     while data:
-        fragmented_data[i] = data
+        fragmented_data.append(data)
         data = f.read(block_size)
-        i += 1
+    f.close()
     return fragmented_data
 
 
 def init_socket():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setblocking(0)
     return sock
 
 
 def handshake(relay_socket, ip_port=(loopback_address, default_udp_port)):
-    relay_socket.setblocking(1)
+    global handshake_message
+
     reply = ''
     while not reply:
         try:
-            relay_socket.send_to(handshake_message, ip_port)
+            relay_socket.sendto(handshake_message, ip_port)
             relay_socket.settimeout(5)
             reply = relay_socket.recvfrom(block_size)[0]
 
@@ -63,18 +68,27 @@ def handshake(relay_socket, ip_port=(loopback_address, default_udp_port)):
             pass
 
     if reply == handshake_word:
-        relay_socket.setblocking(0)
         return True
     return False
 
 
 def send_queued_packets(packets_dictionary, relay_socket, ip_port):
     for packet in packets_dictionary:
-        packet.send_packet(relay_socket, ip_port)
+        time.sleep(default_time_between_send)
+        packets_dictionary[packet].send_packet(relay_socket, ip_port)
 
+def create_packets(fragmented_data):
+    i = 0
+    packets = {}
+    fragment_size = len(fragmented_data)
+    for fragment in fragmented_data:
+        packet = Packet(i, fragment, fragment_size)
+        packets[i] = packet
+        i += 1
+    return packets
 
 def main():
-    relay_ip_address = sys.argv[0]
+    relay_ip_address = sys.argv[1]
     sock = init_socket()
 
     fragmented_data = read_file_and_fragment_data()
@@ -84,12 +98,8 @@ def main():
         print '[!] Was unable to establish connection with relay server...'
         sys.exit()
 
-    i = 0
-    packets = {}
-    fragment_size = len(fragmented_data)
-    for fragment in fragmented_data:
-        packet = Packet(i, fragment, fragment_size)
-        packets[i] = packet
+    packets = create_packets(fragmented_data)
+    print len(packets)
 
     done = False
     retransmit = True
@@ -98,11 +108,21 @@ def main():
             send_queued_packets(packets, relay_socket=sock, ip_port=(relay_ip_address, default_udp_port))
             retransmit = False
             timestamp = time.time()
-        last_received_packet_raw = sock.recvfrom(received_buffer_size)[0]
-        last_received_ack = struct.unpack('B', last_received_packet_raw)
+        try:
+            last_received_packet_raw = sock.recvfrom(received_buffer_size)[0]
+            last_received_ack = struct.unpack('B', last_received_packet_raw)[0]
+            if last_received_ack == done_sequence_number:
+                done = True
+                sys.exit(0)
+            del packets[last_received_ack]
+        except socket.timeout:
+            pass
+        finally:
+            if time.time() - timestamp > default_wait_time:
+                retransmit = True
+            if not packets:
+                done = True
 
-        del packets[last_received_ack]
-        if time.time() - timestamp > default_wait_time:
-            retransmit = False
-        if not packets:
-            done = True
+
+if __name__ == '__main__':
+    main()
--- satellite.py
+++ satellite.py
@@ -8,61 +8,45 @@ import struct
 
 default_udp_port                       = 10000
 loopback_address                       = '127.0.0.1'
-default_filename                       = 'input.txt'
+default_filename                       = 'output.txt'
 block_size                             = 500 - 20 - 8
-received_buffer_size                   = 20 + 8 + 4
-seq_mask                               = 0xffffffffff
 handshake_message                      = 'yo'
 default_wait_time                      = 2
 handshake_word                         = 'hello'
-
-
-class Packet(object):
-    def __init__(self, seq_number, data):
-        self.__sequence_number = seq_number
-        self.__raw_data = data
-        self.__data = None
-        self._generate_payload()
-
-    def _generate_payload(self):
-        seq_header = seq_mask ^ self.__sequence_number
-        self.__data = struct.pack('ls', seq_header, self.__raw_data)
-
-    def send_packet(self, relay_socket, ip_port=(loopback_address, default_udp_port)):
-        relay_socket.send_to(self.__data, ip_port)
+done_sequence_number                   = 255
 
 
 def init_socket():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setblocking(0)
     return sock
 
 
 def handshake(relay_socket, ip_port=(loopback_address, default_udp_port)):
-    relay_socket.setblocking(1)
     reply = ''
     while not reply:
         try:
-            relay_socket.send_to(handshake_message, ip_port)
+            relay_socket.sendto(handshake_message, ip_port)
             relay_socket.settimeout(5)
             reply = relay_socket.recvfrom(block_size)[0]
-
         except socket.timeout:
             pass
 
     if reply == handshake_word:
-        relay_socket.setblocking(0)
         return True
     return False
 
+def reassemble_file(fragmented_data):
+    output = open(default_filename, 'wb')
+    for fragment in fragmented_data:
+        output.write(fragmented_data[fragment])
+    output.close()
 
-def send_ack(sequence_number, relay_socket, ip_port):
-    seq_ack_to_send = seq_mask ^ sequence_number
-    relay_socket.send_to(seq_ack_to_send, ip_port)
-
+def response_ack(relay_sock, ip_port, sequence_number):
+    data_to_send = struct.pack('B', sequence_number)
+    relay_sock.sendto(data_to_send, ip_port)
 
 def main():
-    relay_ip_address = sys.argv[0]
+    relay_ip_address = sys.argv[1]
     sock = init_socket()
 
     connection_established = handshake(sock, ip_port=(relay_ip_address, default_udp_port))
@@ -70,3 +54,23 @@ def main():
         print '[!] Was unable to establish connection with relay server...'
         sys.exit()
 
+    fragmented_data = {}
+    done = False
+    while not done:
+        try:
+            received_raw_data = sock.recvfrom(block_size)[0]
+            number_of_fragments, sequence_number = struct.unpack('BB', received_raw_data[:2])
+            actual_data = received_raw_data[2:]
+            fragmented_data[sequence_number] = actual_data
+            response_ack(sock, (relay_ip_address, default_udp_port), sequence_number)
+        except socket.timeout:
+            pass
+
+        if number_of_fragments == len(fragmented_data):
+            done = True
+            response_ack(sock, (relay_ip_address, default_udp_port), done_sequence_number)
+
+    reassemble_file(fragmented_data)
+
+if __name__ == '__main__':
+    main()
